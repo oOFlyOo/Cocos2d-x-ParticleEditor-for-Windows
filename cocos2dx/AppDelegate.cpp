7f55576ad408f6012ff790c6f49098934b722fbb
Fly <393830887@qq.com>
2014-09-24 21:11:11 +0800

    修改默认大小为960 * 640

--- cocos2dx/AppDelegate.cpp
+++ cocos2dx/AppDelegate.cpp
@@ -25,7 +25,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     pDirector->setOpenGLView(pEGLView);
 
     // Set the design resolution
-    pEGLView->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, kResolutionNoBorder);
+//     pEGLView->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, kResolutionNoBorder);
 
 	CCSize frameSize = pEGLView->getFrameSize();
     
@@ -37,26 +37,26 @@ bool AppDelegate::applicationDidFinishLaunching() {
     // this can make sure that the resource's height could fit for the height of design resolution.
 
     // if the frame's height is larger than the height of medium resource size, select large resource.
-	if (frameSize.height > mediumResource.size.height)
-	{
-        searchPath.push_back(largeResource.directory);
-
-        pDirector->setContentScaleFactor(MIN(largeResource.size.height/designResolutionSize.height, largeResource.size.width/designResolutionSize.width));
-	}
+// 	if (frameSize.height > mediumResource.size.height)
+// 	{
+//         searchPath.push_back(largeResource.directory);
+// 
+//         pDirector->setContentScaleFactor(MIN(largeResource.size.height/designResolutionSize.height, largeResource.size.width/designResolutionSize.width));
+// 	}
     // if the frame's height is larger than the height of small resource size, select medium resource.
-    else if (frameSize.height > smallResource.size.height)
-    {
-        searchPath.push_back(mediumResource.directory);
-        
-        pDirector->setContentScaleFactor(MIN(mediumResource.size.height/designResolutionSize.height, mediumResource.size.width/designResolutionSize.width));
-    }
-    // if the frame's height is smaller than the height of medium resource size, select small resource.
-	else
-    {
-        searchPath.push_back(smallResource.directory);
-
-        pDirector->setContentScaleFactor(MIN(smallResource.size.height/designResolutionSize.height, smallResource.size.width/designResolutionSize.width));
-    }
+//     else if (frameSize.height > smallResource.size.height)
+//     {
+//         searchPath.push_back(mediumResource.directory);
+//         
+//         pDirector->setContentScaleFactor(MIN(mediumResource.size.height/designResolutionSize.height, mediumResource.size.width/designResolutionSize.width));
+//     }
+//     // if the frame's height is smaller than the height of medium resource size, select small resource.
+// 	else
+//     {
+//         searchPath.push_back(smallResource.directory);
+// 
+//         pDirector->setContentScaleFactor(MIN(smallResource.size.height/designResolutionSize.height, smallResource.size.width/designResolutionSize.width));
+//     }
     
     // set searching path
     CCFileUtils::sharedFileUtils()->setSearchPaths(searchPath);
@@ -65,6 +65,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     pDirector->setDisplayStats(true);
 
     // set FPS. the default value is 1.0/60 if you don't call this
+    //改为默认30帧每秒
     pDirector->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
--- cocos2dx/ExportAPI.cpp
+++ cocos2dx/ExportAPI.cpp
@@ -18,10 +18,10 @@ extern "C"
 
 		CCEGLView* eglView = CCEGLView::sharedOpenGLView();
 		eglView->setViewName("HelloCpp");
-		eglView->setFrameSize(1024, 768);
+		eglView->setFrameSize(960, 640);
 		// The resolution of ipad3 is very large. In general, PC's resolution is smaller than it.
 		// So we need to invoke 'setFrameZoomFactor'(only valid on desktop(win32, mac, linux)) to make the window smaller.
-		eglView->setFrameZoomFactor(1.f);
+		eglView->setFrameZoomFactor(1);
 
 
 		CCApplication::sharedApplication()->run();
